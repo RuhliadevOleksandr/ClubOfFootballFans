b5c964153b3e121969ef5cff77be00f41292cdaa
Oleksandr Ruhliadev <sasharuhliadev2@gmail.com>
2021-05-14 13:05:28 +0300

    Add project files.

---
+++ Committee/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FootballFans
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+    class Committee
+    {
+
+    }
+}
