0860972e6c97f5e32762e1079e1d7cb848c48226
Oleksandr Ruhliadev <sasharuhliadev2@gmail.com>
2021-11-22 12:03:22 +0200

    Changed exceptions

--- FootballFansLib/Person.cs
+++ FootballFansLib/Person.cs
@@ -7,24 +7,19 @@ namespace FootballFansLib
         public string Surname { get; private set; }
         public Person(string surname)
         {
-            if (surname != null)
+            if (!string.IsNullOrEmpty(surname))
             {
                 bool isCorrectSurname = true;
-                if (surname == "")
-                    throw new ArgumentException("\nSurname must have letters!");
-                else
-                {
-                    for (int i = 0; i < surname.Length; i++)
+                for (int i = 0; i < surname.Length; i++)
                         if (!Char.IsLetter(surname[i]))
                             isCorrectSurname = false;
-                }
                 if (isCorrectSurname)
                     Surname = surname;
                 else
                     throw new ArgumentException("\nSurname must have only letters!");
             }
             else
-                throw new NullReferenceException("\nPerson must have surname!");
+                throw new ArgumentException("\nPerson must have surname! And surname can't be empty");
         }
     }
 }
