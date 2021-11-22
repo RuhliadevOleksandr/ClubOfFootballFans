4474dacf4a4b078699ea1b935e77c3e64518e444
Oleksandr Ruhliadev <sasharuhliadev2@gmail.com>
2021-11-22 12:05:53 +0200

    Changed properties FavouritePlayer, FavouriteTeam

--- FootballFansLib/FootballFan.cs
+++ FootballFansLib/FootballFan.cs
@@ -6,59 +6,45 @@ namespace FootballFansLib
     {
         private string _favouritePlayer;
         private string _favouriteTeam;
-        public FootballFan(string surname) : base(surname) {}
         public string FavouritePlayer
         {
-            get
-            {
-                return _favouritePlayer;
-            }
+            get { return _favouritePlayer; }
             set
             {
-                bool isCorrectSurname = true;
-                if (value == "" || value == null)
-                {
+                if (string.IsNullOrEmpty(value))
                     _favouritePlayer = null;
-                    throw new NullReferenceException("\nYou haven't got a favourite player!");
-                }
                 else
-                {
-                    for (int i = 0; i < value.Length; i++)
-                        if (!Char.IsLetter(value[i]))
-                            isCorrectSurname = false;
+                {  
+                    if (isCorrectName(value))
+                        _favouritePlayer = value;
+                    else
+                        throw new ArgumentException("\nSurname of player must have only letters!");
                 }
-                if (isCorrectSurname)
-                    _favouritePlayer = value;
-                else
-                    throw new ArgumentException("\nSurname of player must have only letters!");
             }
         }
         public string FavouriteTeam
         {
-            get
-            {
-                return _favouriteTeam;
-            }
+            get { return _favouriteTeam; }
             set
             {
-                bool isCorrectSurname = true;
-                if (value == "" || value == null)
-                {
+                if (string.IsNullOrEmpty(value))
                     _favouriteTeam = null;
-                    throw new NullReferenceException("\nYou haven't got a favourite team!");
-                }
                 else
                 {
-                    for (int i = 0; i < value.Length; i++)
-                        if (!Char.IsLetter(value[i]))
-                            isCorrectSurname = false;
+                    if (isCorrectName(value))
+                        _favouriteTeam = value;
+                    else
+                        throw new ArgumentException("\nName of team must have only letters!");
                 }
-                if (isCorrectSurname)
-                    _favouriteTeam = value;
-                else
-                    throw new ArgumentException("\nName of team must have only letters!");
-
             }
         }
+        private bool isCorrectName(string name)
+        {
+            for (int i = 0; i < name.Length; i++)
+                        if (!Char.IsLetter(name[i]))
+                            return false;
+            return true;
+        }
+        public FootballFan(string surname) : base(surname) {}
     }
 }
--- FootballFansLib/FootballPlayer.cs
+++ FootballFansLib/FootballPlayer.cs
@@ -4,8 +4,47 @@ namespace FootballFansLib
 {
     public class FootballPlayer : Person, IRootable
     {
-        public string FavouritePlayer { get; set; }
-        public string FavouriteTeam { get; set; }
+        private string _favouritePlayer;
+        private string _favouriteTeam;
+        public string FavouritePlayer
+        {
+            get { return _favouritePlayer; }
+            set
+            {
+                if (string.IsNullOrEmpty(value))
+                    _favouritePlayer = null;
+                else
+                {  
+                    if (isCorrectName(value))
+                        _favouritePlayer = value;
+                    else
+                        throw new ArgumentException("\nSurname of player must have only letters!");
+                }
+            }
+        }
+        public string FavouriteTeam
+        {
+            get { return _favouriteTeam; }
+            set
+            {
+                if (string.IsNullOrEmpty(value))
+                    _favouriteTeam = null;
+                else
+                {
+                    if (isCorrectName(value))
+                        _favouriteTeam = value;
+                    else
+                        throw new ArgumentException("\nName of team must have only letters!");
+                }
+            }
+        }
+        private bool isCorrectName(string name)
+        {
+            for (int i = 0; i < name.Length; i++)
+                        if (!Char.IsLetter(name[i]))
+                            return false;
+            return true;
+        }
         public FootballPlayer(string surname) : base(surname) { }
     }
 }
