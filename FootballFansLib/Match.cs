5073d77748b954ec35761fd91c1c62536e88b364
Oleksandr Ruhliadev <sasharuhliadev2@gmail.com>
2021-11-22 12:10:09 +0200

    Changed class Match to collection of 2 team

--- FootballFansLib/Match.cs
+++ FootballFansLib/Match.cs
@@ -1,7 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 
 namespace FootballFansLib
 {
@@ -10,20 +8,20 @@ namespace FootballFansLib
         public enum Types { ChampionsLeague, EuropaLeague, EURO }
         public Types TypeOfMatch { private set; get; }
         public DateTime DateOfTheMatch { private set; get; }
-        public FootballTeam[] MembersOfTheMatch { private set; get; }
+        public List<FootballTeam> MembersOfTheMatch { private set; get; }
         public enum Result { Win, Draw, Lose }
         public Match(FootballTeam team, FootballTeam team2, DateTime dateOfTheMatch, Types type)
         {
             if (dateOfTheMatch != null)
             {
+                MembersOfTheMatch = new List<FootballTeam>();
+                MembersOfTheMatch.Add(team);
+                MembersOfTheMatch.Add(team2);
                 DateOfTheMatch = dateOfTheMatch;
-                MembersOfTheMatch = new FootballTeam[2];
-                MembersOfTheMatch[0] = team;
-                MembersOfTheMatch[1] = team2;
                 TypeOfMatch = type;
             }
             else
-                throw new NullReferenceException("Match need to have date!");
+                throw new NullReferenceException("\nMatch need to have date!");
         }
         public void EndOfTheMatch(Result result)
         {
